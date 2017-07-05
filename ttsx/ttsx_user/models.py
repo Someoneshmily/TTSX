844c34c08ad56c5851d4a67d7bbe3be9023476a0
Someoneshmily <1294118529@qq.com>
2017-07-05 16:12:38 +0800

    页面继承base开始创建

--- ttsx/ttsx_user/models.py
+++ ttsx/ttsx_user/models.py
@@ -1,3 +1,15 @@
+# coding=utf-8
 from django.db import models
 
 # Create your models here.
+
+
+class UserInfo(models.Model):
+
+    uname = models.CharField(max_length=20)
+    upub = models.CharField(max_length=40)  # sha1
+    umail = models.CharField(max_length=30)
+    urecieve = models.CharField(max_length=20)  # 接收人
+    uaddress = models.CharField(max_length=100)  # 地址
+    ucode = models.CharField(max_length=6)  # 编码
+    uphone = models.CharField(max_length=11)  # 机号
